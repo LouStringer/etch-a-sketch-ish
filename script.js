3bd40d9346f335621ab25941e0b8c529b1c0988a
Lou Stringer <louise.stringer@gmail.com>
2018-12-14 21:15:35 +0000

    add functionality to change colour of track and some minor styling

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-//setup (with default row size)
-const sketchpad = document.querySelector("#sketchpad");
-
-// change colour function
-const changeColour = function() {
-  this.classList.add('mouseovered')
-}
-
 // generate & size sketchpad inner divs
+const sketchpad = document.querySelector("#sketchpad");
 const generateDivs = (rowSize = 16) => {
   sketchpad.innerHTML = `<div class = "sketchpadDiv"></div>`.repeat(`${rowSize*rowSize}`);
   const size = sketchpad.clientHeight / rowSize
   document.querySelectorAll(".sketchpadDiv").forEach(item => {
     item.style.setProperty('--size', `${size}px`);
-    item.addEventListener('mouseover', changeColour);
+    item.addEventListener('mouseover', changePixelColour);
   });
 }
 
-// reset button
+// change colour function
+const singleColour = document.querySelector('#single');
+const multiColour = document.querySelector('#multi');
+const possColours = ['#D5DDE7', '#CCD3D8', '#CFF0E0', '#D9F0EB', '#EFD9EF', '#E4D5F0', '#DCD0E6', '#D0E1F4', '#D6DDF4', '#F4D8D8'];
+let currentColour = '#D5DDE7';
+
+const singleColourSetting = function() {
+    currentColour = possColours[Math.floor(Math.random()*10)]
+}
+singleColour.addEventListener('click', singleColourSetting);
+
+const changePixelColour = function() {
+  singleColour.checked ? this.style.backgroundColor = currentColour : this.style.backgroundColor = possColours[Math.floor(Math.random()*10)];
+}
+
+// reset colour button
 const resetButton = document.querySelector("button.reset");
 const resetColour = function() {
-  document.querySelectorAll(".sketchpadDiv").forEach(item => item.classList.remove('mouseovered'))
+  document.querySelectorAll(".sketchpadDiv").forEach(item => item.style.backgroundColor = '#ffffff')
 }
 resetButton.addEventListener('click', resetColour);
 
